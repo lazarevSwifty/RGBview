fb7e4997d874b894cc348ffda660c7c2835159b6
vladislav <vladlazarevdev@mail.ru>
2019-07-27 18:18:24 +0300

    Initial Commit

---
+++ RGBview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGBview
+//
+//  Created by Владислав Лазарев on 27/07/2019.
+//  Copyright © 2019 Владислав Лазарев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
